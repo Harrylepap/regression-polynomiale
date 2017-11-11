14948fd6a43324b98b189fb435c3af3edffa7f19
harrylepap <rakotondratompobakoharry@yahoo.fr>
2017-11-11 21:16:31 +0300

    Return Final

--- regression.py
+++ regression.py
@@ -12,10 +12,6 @@ tempsChargementPages = np.random.normal(3.0, 1.0, 1000)
 génération aléatoires de montants d'achat corrélés aux temps de chargement
 montantAchat = np.random.normal(50.0, 10.0, 1000) / (tempsChargementPages * tempsChargementPages)
 
-# le temps de chargement de la page est notre variable prédictive
-x = np.array(tempsChargementPages)
-# montantAchat est la variable cible (qu'on cherche à prédire)
-y = np.array(montantAchat)
 
 axes = plt.axes()
 axes.grid()
@@ -24,7 +20,14 @@ plt.ylabel('montant des achats euros')
 plt.scatter(tempsChargementPages, montantAchat)
 plt.show()
 
+# le temps de chargement de la page est notre variable prédictive
+x = np.array(tempsChargementPages)
+# montantAchat est la variable cible (qu'on cherche à prédire)
+y = np.array(montantAchat)
+
 p4 = np.poly1d(np.polyfit(x, y, 4))
 #print p4
 xp = np.linspace(0, 7, 100)
+plt.scatter(c, y)
 plt.plot(xp, p4(xp), c='r')
+plt.show()
